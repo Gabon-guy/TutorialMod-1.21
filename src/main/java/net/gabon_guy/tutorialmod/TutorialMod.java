06eebebcf77551af06c1358633ccc61e52d86ec2
Brandon <minecraftcreeperpro123@gmail.com>
2024-12-22 14:30:16 -0800

    Mod Items

--- src/main/java/net/gabon_guy/tutorialmod/TutorialMod.java
+++ src/main/java/net/gabon_guy/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.gabon_guy.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.gabon_guy.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,7 +10,10 @@ public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+
+
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 	}
 }
