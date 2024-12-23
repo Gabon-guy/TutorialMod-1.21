12177588d2a04f83ce35fdb4a1941df2a32adc2c
Brandon <minecraftcreeperpro123@gmail.com>
2024-12-22 18:04:43 -0800

    ItemGroups andd small fixes

--- src/main/java/net/gabon_guy/tutorialmod/TutorialMod.java
+++ src/main/java/net/gabon_guy/tutorialmod/TutorialMod.java
@@ -2,12 +2,14 @@ package net.gabon_guy.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.gabon_guy.tutorialmod.block.ModBlocks;
+import net.gabon_guy.tutorialmod.item.ModItemGroups;
 import net.gabon_guy.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
-	public static final String MOD_ID = "tutorialmod";
+	public static final String MOD_ID = "data/tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 
@@ -15,5 +17,7 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
+		ModItemGroups.registerItemGroups();
 	}
 }
