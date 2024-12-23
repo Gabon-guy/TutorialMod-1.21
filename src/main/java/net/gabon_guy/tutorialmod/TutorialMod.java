e18cee4e8a4c1279039c3f0ef2a300c8e3e6fe02
Brandon <minecraftcreeperpro123@gmail.com>
2024-12-23 08:37:33 -0800

    Loot tables and the "mineable" tag

--- src/main/java/net/gabon_guy/tutorialmod/TutorialMod.java
+++ src/main/java/net/gabon_guy/tutorialmod/TutorialMod.java
@@ -9,10 +9,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
-	public static final String MOD_ID = "data/tutorialmod";
+	public static final String MOD_ID = "tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-
 
 	@Override
 	public void onInitialize() {
